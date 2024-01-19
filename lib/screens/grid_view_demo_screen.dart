9764669d3bdabc633e4337b18703a889a3af6314
Abid <abidroid1@gmail.com>
2024-01-19 17:10:41 +0500

    gridview screen added

---
+++ lib/screens/grid_view_demo_screen.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class GridViewDemoScreen extends StatelessWidget {
+  const GridViewDemoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
